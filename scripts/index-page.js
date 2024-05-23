045395318242a034bd334a865fc96278ed92b394
Lindsay Renna <lrenna.oct@gmail.com>
2024-05-22 22:23:26 -0400

    building comment DOM manipulation

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,46 +1,46 @@
-const commentData = [
+const comments = [
 	{
 		name: "Victor Pinto",
 		comment:
 			"This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
 		date: new Date(2023, 10, 2),
-		userImage: null,
+		image: null,
 	},
 	{
 		name: "Christina Cabrera",
 		comment:
 			"I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
 		date: new Date(2023, 9, 28),
-		userImage: null,
+		image: null,
 	},
 	{
 		name: "Isaac Tadesse",
 		comment:
 			"I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
 		date: new Date(2023, 9, 20),
-		userImage: null,
+		image: null,
 	},
 ];
 
-console.log(commentData);
+console.log(comments);
 
-/*
-<div class="posted-comments">
-    <div class="user-image">
-        <div class="null-image"></div>
-    </div>
-    <div class="comments__wrapper">
-        <h3 class="user-name">Lindsay Renna</h3>
-        <p class="time-stamp">11/02/2023</p>
-        <p class="user-comment">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Officiis rerum soluta fugiat odio explicabo, reiciendis odit,
-            repellat quam cumque iusto rem amet esse labore sapiente
-            adipisci? Delectus consectetur illum voluptatum!
-        </p>
-    </div>
+{
+	/* <div class="posted-comments">
+<div class="user-image">
+    <div class="null-image"></div>
+</div>
+<div class="comments__wrapper">
+    <h3 class="user-name">Lindsay Renna</h3>
+    <p class="time-stamp">11/02/2023</p>
+    <p class="user-comment">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis
+        rerum soluta fugiat odio explicabo, reiciendis odit, repellat quam
+        cumque iusto rem amet esse labore sapiente adipisci? Delectus
+        consectetur illum voluptatum!
+    </p>
 </div>
 </div> */
+}
 
 const newCommentForm = document.querySelector(".new-comment-form");
 
@@ -65,13 +65,14 @@ newCommentForm.addEventListener("submit", (event) => {
 		name: userName,
 		comment: commentText,
 		date: new Date(),
-		userImage: "./assets/Images/Mohan-muruge.jpg",
+		userImage: "/Mohan-muruge.jpg",
 	};
 
-	commentData.push(newComment);
+	comments.push(newComment);
 	newCommentForm.reset();
+	postComment(comments[comments.length - 1]);
 
-	console.log(commentData);
+	console.log(comments);
 
 	// Form Validation
 
@@ -91,5 +92,52 @@ newCommentForm.addEventListener("submit", (event) => {
 });
 
 // make past-comments as an object of username, timestamp, comment and user image
-// push new comment data to the array of comments
 // do a loop over the array to create the comments
+
+const postComment = (comment) => {
+	const commentList = document.querySelector(".posted-comments");
+
+	// add user profile image
+	const imageContainer = document.createElement("div");
+	imageContainer.classList.add("user-image");
+	commentList.appendChild(imageContainer);
+
+	if (comment.image === null) {
+		const imageElement = document.createElement("div");
+		imageElement.classList.add("null-img");
+		commentList.appendChild(imageElement);
+	} else {
+		const imageElement = document.createElement("img");
+		imageElement.classList.add("img");
+		imageElement.setAttribute("src", `./Assets/Icons/SVG/${comment.image}`);
+		commentList.appendChild(imageElement);
+	}
+
+	// add comments data
+	const commentContainer = document.createElement("div");
+	commentContainer.classList.add("comments__wrapper");
+	commentList.appendChild(commentContainer);
+
+	// user name data
+	const commenterName = document.createElement("h3");
+	commenterName.classList.add("user-name");
+	commenterName.textContent = comment.name;
+	commentContainer.appendChild(commenterName);
+
+	// time-stamp data
+	const commentTime = document.createElement("p");
+	commentTime.classList.add("time-stamp");
+	commentTime.textContent = comment.date;
+	commentContainer.appendChild(commentTime);
+
+	// user comment data
+	const commentText = document.createElement("p");
+	commentText.classList.add("user-comment");
+	commentText.textContent = comment.comment;
+	commentContainer.appendChild(commentText);
+};
+
+postComment(comments[0]);
+postComment(comments[1]);
+postComment(comments[2]);
+postComment(comments[3]);
